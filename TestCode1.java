ba7f6acc63b5ba1af6debe8ff2239f4bb44e4f71
Priyanshilakshkar <168474934+Priyanshilakshkar@users.noreply.github.com>
2026-09-25 03:37:16 +0530

    Add TestCode1.java to calculate sum of an array

---
+++ TestCode1.java
@@ -0,0 +1,24 @@
+package com.example;
+
+public class TestCode {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        int sum = calculateSum(numbers);
+
+        System.out.println("Sum = " + sum);
+    }
+
+    public static int calculateSum(int[] numbers) {
+
+        int sum = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+        }
+
+        return sum;
+    }
+}
